16bd0f1b1ec3e1e2aa6c1bd8088ba5f5c1990df6
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-08-17 20:24:10 +0200

    basename added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router} from 'react-router-dom'
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Router>
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>
     <App/>
 </Router>, rootElement);
