311920ae4b9e192e804ba74d5322f44100308b4e
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-08-25 09:31:07 +0200

    search and display logic deplaced into dedicated components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React, {Component} from "react";
-import Entries from "./Entries";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import NavBar from './components/NavBar';
-import stateStore from './stateStore'
+import React from "react";
 import {view} from 'react-easy-state';
 import {withRouter} from 'react-router-dom';
+import SearchResults from "./components/SearchResults";
+import SearchSettings from "./components/SearchSettings";
 
 
-class App extends Component {
+class App extends React.Component {
 
-
-    get renderEntries() {
-        let entries = <h1>No entries found</h1>;
-        if (stateStore.search.entries) {
-            entries = <Entries list={stateStore.search.entries}/>;
-        }
-        return entries;
-    }
 
     render() {
         return (
             <div id="app">
-                <NavBar/>
-                <Container fluid="md">
-                    <Row> {this.renderEntries}</Row>
-                </Container>
+                <SearchSettings/>
+                <SearchResults/>
             </div>
         );
     }
