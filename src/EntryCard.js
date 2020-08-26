8cdaa21261eef3be34016e69b735045b4f0f24e9
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-08-26 13:50:36 +0200

    pretty printing xml

--- src/EntryCard.js
+++ src/EntryCard.js
@@ -11,25 +11,28 @@ const EntryCard = props => {
     var row = Object.keys(props.item).map(k => {
 
         if (stateStore.results.display_fields[k] === true) {
-
             if (Array.isArray(props.item[k])) {
-
-
                 return <div key={k}>
                     <Badge variant="primary">{k}:</Badge>
                     <div>
                         {props.item[k].map(r => (<li key={r}>{r}</li>))}</div>
                 </div>;
-
             } else {
-
-                return <div key={k}>
-                    <Badge variant="primary">{k}:</Badge>
-                    <div>{props.item[k]}</div>
-                </div>
-
+                if (k === "xml") {
+                    const format = require('xml-formatter');
+                    let xml = props.item[k];
+                    let formattedXml = format(xml);
+                    return <div key={k}>
+                        <Badge variant="primary">{k}:</Badge>
+                        <pre>{formattedXml}</pre>
+                    </div>
+                } else {
+                    return <div key={k}>
+                        <Badge variant="primary">{k}:</Badge>
+                        <div>{props.item[k]}</div>
+                    </div>
+                }
             }
-
         } else {
             return null;
         }
