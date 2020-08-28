2ae7739f9c188cbec7a435221c251f053de0a3ae
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-08-28 11:13:03 +0200

    dynamic id generation

--- src/components/CustomSelect.js
+++ src/components/CustomSelect.js
@@ -1,4 +1,4 @@
-import {Form} from  "react-bootstrap";
+import {Form} from "react-bootstrap";
 import React from "react";
 import {withRouter} from "react-router-dom";
 import {view} from "react-easy-state";
@@ -9,12 +9,13 @@ const CustomSelect = ({list, onc, label, preselected}) => {
         return null;
     }
     return (
-        <Form  inline>
-            <Form.Group controlId="exampleForm.SelectCustom">
+        <Form inline>
+            <Form.Group controlId={'select_' + label}>
                 <Form.Label className="mr-1 ml-1">{label}</Form.Label>
                 <Form.Control value={preselected} as="select" onChange={v => onc(v)} custom>
                     {list.map(v => (
                         <option
+                            id={'ID_' + v}
                             key={'quick_field_' + v}
                             value={v}
                             className="secondary-font">
