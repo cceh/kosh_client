da353daf51d3378c817fe0162d20ec50ef3cdb99
Moritz Esser <messer19@uni-koeln.de>
2023-01-09 11:18:18 +0100

    Upate: Auto Select all Dicts

--- src/components/MainSearchSettings.js
+++ src/components/MainSearchSettings.js
@@ -16,46 +16,50 @@ class MainSearchSettings extends React.Component {
 
     default_setup() {
         stateStore.dict_collection.base_path = "mpcd";
-        stateStore.dict_collection.dict_id = ["mmp"];
+        stateStore.dict_collection.dict_id = stateStore.mpcd_ids;
         stateStore.dict_collection.dict_ids = stateStore.mpcd_ids;
         stateStore.search.field = "trc";
         stateStore.search.query_type = "wildcard"
     }
 
     componentDidMount() {
+        this.setMultiSpec()
         this.setSpec()
     }
 
-    setSpec() {
+    setMultiSpec() {
         let base_url = 'https://kosh.uni-koeln.de/'
-        
-        const spec_url = base_url + stateStore.dict_collection.base_path + `/` + stateStore.dict_collection.dict_id + `/restful/spec`;
-        stateStore.dict_collection.dict_base_url = base_url + stateStore.dict_collection.base_path + `/` + stateStore.dict_collection.dict_id + `/restful`
-        console.log(spec_url)
-        axios.get(spec_url).then(resp => {
-            stateStore.dict_spec.raw = resp.data;
-            stateStore.search.fields = stateStore.dict_spec.raw['paths']['/entries']['get']['parameters'][0]['enum'];
-            //remove created
-            stateStore.search.fields = stateStore.search.fields.filter(item => item !== 'created')
-            console.log(stateStore.search.fields.indexOf('trc'))
-            let temp = ['trc'];
-
-            for(var i in stateStore.search.fields){
-                if(stateStore.search.fields[i] !== 'trc'){
-                    temp.push(stateStore.search.fields[i])
-                }
+        stateStore.dict_collection.dict_ids.map((id)=>
+            {
+                const spec_url = base_url + stateStore.dict_collection.base_path + `/` + id + `/restful/spec`;
+                console.info(spec_url)
+                axios.get(spec_url).then(resp => {
+                    console.log(resp.data)
+                    stateStore.search.fields = [...new Set([...stateStore.search.fields,...resp.data['paths']['/entries']['get']['parameters'][0]['enum']])]
+                    console.log(stateStore.search.fields)
+                }).catch((error) => {
+                    console.warn('error fetching spec');
+                    console.error(error)
+                })
             }
+        )
+    }
+
+    setSpec() {
+        stateStore.search.fields = stateStore.search.fields.filter(item => item !== 'created')
+        let temp = ['trc'];
+
+        for(var i in stateStore.search.fields){
+            if(stateStore.search.fields[i] !== 'trc'){
+                temp.push(stateStore.search.fields[i])
+            }
+        }
 
             // set default fields to be displayed
             stateStore.results.display_fields = this.initFields(temp)
             stateStore.results.display_fields['xml'] = false
             stateStore.results.display_fields['id'] = false
-            console.log(stateStore.results.display_fields);
-            stateStore.search.query_types = stateStore.dict_spec.raw['paths']['/entries']['get']['parameters'][2]['enum'];
-        }).catch((error) => {
-            console.warn('error fetching spec');
-        })
-
+            // stateStore.search.query_types = stateStore.dict_spec.raw['paths']['/entries']['get']['parameters'][2]['enum'];
     }
 
     initFields(fields) {
@@ -120,7 +124,7 @@ class MainSearchSettings extends React.Component {
         return (
             <>
             <Navbar expand="lg" sticky="top" className="bg-light">
-            <CustomMultiselect labels={stateStore.mpcd_ids} preselected={stateStore.dict_collection.dict_id} onc={this.selectedOptions}/>
+            <CustomMultiselect labels={stateStore.mpcd_ids} preselected={stateStore.dict_collection.dict_ids} onc={this.selectedOptions}/>
             </Navbar>
             <Navbar expand="lg" sticky="top" className="bg-light">
                 <CustomSelect list={stateStore.search.fields}
