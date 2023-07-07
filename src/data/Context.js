04aed1fa89a60e07c57341b11ff57b0ed0589744
lguenth <mail@lukeguenther.de>
2023-07-07 16:19:14 +0200

    Set proper default values

--- src/data/Context.js
+++ src/data/Context.js
@@ -15,20 +15,20 @@ const stateStore = {
   search: {
     value: "", // String that's been input by the user
     query: "", // Query URL
-    dicts: [],
+    dicts: ["de_alcedo"],
     field: "lemma", // Active field to be searched
-    query_type: "wildcard", // Active query type
+    query_type: "prefix", // Active query type
     query_size: 20, // Active query size
     query_sizes: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100], // All available query sizes
   },
 
   collection: {
-    id: "mpcd",
+    id: "Kosh Data",
     base_path: "api", // Default collection to search
     dicts: [],
     dict_base_urls: {}, // All base REST/GraphQL URLs for all dictionaries
     dict_ids: [], // All available dictionaries in collection
-    dict_fields: [], // All available fields for all dictionaries
+    dict_fields: ["lemma", "sense"], // All available fields for all dictionaries
     dict_types: [], // All available query types for all dictionaries
     exclude: ["cpd", "mmp", "nmp"],
   },
