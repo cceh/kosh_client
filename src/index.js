d824ea51771b3a0b20527fd64a0a214ad8cb1ece
lguenth <mail@lukeguenther.de>
2023-07-07 15:51:09 +0200

    Run react-codemod

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -6,7 +6,7 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
