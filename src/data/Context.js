b062fb53ac24051ae915d90ba93109d6e439b1c6
lguenth <mail@lukeguenther.de>
2023-07-17 14:11:21 +0200

    Rephrase context clues

--- src/data/Context.js
+++ src/data/Context.js
@@ -13,30 +13,30 @@ const stateStore = {
   ],
 
   search: {
-    value: "", // String that's been input by the user
+    value: "", // Query string
     query: "", // Query URL
-    dicts: ["de_alcedo"],
+    dicts: ["de_alcedo"], // Active dictionary
     field: "lemma", // Active field to be searched
     query_type: "prefix", // Active query type
     query_size: 20, // Active query size
-    query_sizes: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100], // All available query sizes
+    query_sizes: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100], // Available query sizes
   },
 
   collection: {
-    id: "Kosh Data",
-    base_path: "api", // Default collection to search
-    dicts: [],
+    id: "Kosh Data", // Default collection to search
+    base_path: "api", // Default endpoint
+    dicts: [], // All dictionary objects
     dict_base_urls: {}, // All base REST/GraphQL URLs for all dictionaries
     dict_ids: [], // All available dictionaries in collection
     dict_fields: ["lemma", "sense"], // All available fields for all dictionaries
     dict_types: [], // All available query types for all dictionaries
-    exclude: ["cpd", "mmp", "nmp"],
+    exclude: ["cpd", "mmp", "nmp"], // Which dictionary IDs to exclude
   },
 
   results: {
     loading: false, // Loading results or not?
-    display_fields: {}, // [search.field]: true or false
-    entries: {}, // Actual entries I get back from the server
+    display_fields: {}, // Which fields to display in the table
+    entries: {}, // All result entries
   },
 };
 
